ebb457530944b14e81522a5c655226e59f021030
lluuiissoo <lluuiissoo@gmail.com>
2016-06-09 17:16:15 -0400

    Completed exercise with unit tests

---
+++ exercism.io/leap.exercism.io/Leap.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leap.exercism.io
+{
+    public class Leap
+    {
+        public bool IsLeap(int year)
+        {
+            bool result = false;
+
+            if (IsXDivisibleByY(year,100) && IsXDivisibleByY(year,400))
+            {
+                result = IsXDivisibleByY(year,4);
+            }
+            else
+            {
+                if (!IsXDivisibleByY(year, 100))
+                    result = IsXDivisibleByY(year, 4);
+            }
+
+            return result;
+        }
+
+        private bool IsXDivisibleByY(int x, int y)
+        {
+            bool result = false;
+            if (x % y == 0)
+            {
+                result = true;
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ exercism.io/tests.leap.exercism.io/LeapTest.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using leap.exercism.io;
+
+[TestFixture]
+public class LeapTest
+{
+    Leap Year;
+
+    [TestFixtureSetUp]
+    public void Init()
+    {
+        Year = new Leap();
+    }
+
+    [Test]
+    public void Valid_leap_year()
+    {
+        Assert.That(Year.IsLeap(1996), Is.True);
+    }
+
+    //[Ignore("Remove to run test")]
+    [Test]
+    public void Invalid_leap_year()
+    {
+        Assert.That(Year.IsLeap(1997), Is.False);
+    }
+
+    //[Ignore("Remove to run test")]
+    [Test]
+    public void Turn_of_the_20th_century_is_not_a_leap_year()
+    {
+        Assert.That(Year.IsLeap(1900), Is.False);
+    }
+
+    //[Ignore("Remove to run test")]
+    [Test]
+    public void Turn_of_the_25th_century_is_a_leap_year()
+    {
+        Assert.That(Year.IsLeap(2400), Is.True);
+    }
+}
