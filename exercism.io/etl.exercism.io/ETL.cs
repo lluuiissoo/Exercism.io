50d7ec536a962605f43cb7a6fdd6a2de58e697f9
lluuiissoo <lluuiissoo@gmail.com>
2016-06-20 17:03:22 -0400

    Completed ETL

---
+++ exercism.io/etl.exercism.io/ETL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace etl.exercism.io
+{
+    public class ETL
+    {
+        Dictionary<int, IList<string>> oldScore;
+
+        public Dictionary<string, int> Transform(Dictionary<int, IList<string>> score)
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            
+            foreach(KeyValuePair<int, IList<string>> dictEntry in score)
+            {
+                int numValue = dictEntry.Key;
+                
+                foreach (string strValue in dictEntry.Value)
+                {
+                    result.Add(strValue.ToLower(), numValue);
+                }
+            }
+
+            return result;
+        }
+    }
+}
