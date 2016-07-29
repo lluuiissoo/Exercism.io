cb36b314f1275f00db745850fe71232c172361ae
lluuiissoo <lluuiissoo@gmail.com>
2016-07-29 09:57:08 -0400

    Adding new projects

---
+++ exercism.io/anagram.exercism.io/Anagram.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anagram.exercism.io
+{
+    public class Anagram
+    {
+        private string _word = string.Empty;
+
+        public Anagram(string word)
+        {
+            _word = word;
+        }
+
+        public string[] Match(string[] words)
+        {
+            List<string> anagrams = new List<string>();
+
+            foreach (string word in words)
+            {
+                if ((word.Length == _word.Length) &&
+                    (word.ToLower() != _word.ToLower()))
+                {
+                    string tempWord = _word.ToLower();
+                    bool isAnagram = true;
+                    foreach (char character in word.ToLower())
+                    {
+                        if (tempWord.Contains(character))
+                        {
+                            tempWord = tempWord.Remove(tempWord.IndexOf(character), 1);
+                        }
+                        else
+                        {
+                            isAnagram = false;
+                            break;
+                        }
+                    }
+
+                    if (isAnagram)
+                        anagrams.Add(word);
+                }
+            }
+
+            return anagrams.ToArray();
+        }
+    }
+}
---
+++ exercism.io/hamming.exercism.io/Hamming.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hamming.exercism.io
+{
+    public class Hamming
+    {
+        public int Compute(string dnaStrand1, string dnaStrand2)
+        {
+            int hammingDistance = 0;
+
+            if ((dnaStrand1.Length == dnaStrand2.Length) &&
+                (dnaStrand1 != dnaStrand2))
+            {
+                for (int i = 0; i < dnaStrand1.Length; i++)
+                {
+                    if (dnaStrand1[i] != dnaStrand2[i])
+                        hammingDistance++;
+                }
+            }
+
+            return hammingDistance;
+        }
+    }
+}
